88057d33a8edcf8516e27fb48030d897232e843e
shaunvaidyan <svaidyan24@gmail.com>
2021-04-08 16:02:40 -0500

    Update run.sh

--- boilerplate/run.sh
+++ boilerplate/run.sh
@@ -4,13 +4,13 @@ source /etc/lsb-release
 
 if [[ "$DISTRIB_ID" -ne "Ubuntu" ]]; then
   echo "No action taken..."
-  echo "Are you sure this is an Ubuntu system?"
+  echo "Is this ditro Ubuntu?"
   exit 1
 fi
 
 if [[ $EUID -ne 0 ]]; then
   echo "No action taken..."
-  echo "This script must be run as root"
+  echo "Script must be run as root"
   exit 1
 fi
 
@@ -22,19 +22,19 @@ rm -f /etc/machine-id
 dbus-uuidgen --ensure=/etc/machine-id
 ln -s /etc/machine-id /var/lib/dbus/
 
-echo "Generating SSH server keys"
+echo "Generating SSH keys"
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa -y
 ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa -y
 ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521 -y
 
-echo "Setting Hostname"
+echo "Setting Hosts in hosts and hostname file"
 sed -i "s/$HOSTNAME/$newHostname/g" /etc/hosts
 sed -i "s/$HOSTNAME/$newHostname/g" /etc/hostname
 hostnamectl set-hostname $newHostname
 
-echo "Done!"
+echo "Done"
 
-read -p "Would you like to reboot the system now? [Y/N]: " confirm &&
+read -p "Reboot the system now? [Y/N]: " confirm &&
   [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 reboot
